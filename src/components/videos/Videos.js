e3e3d05165df4eaac5306021b5663914d1381dbf
David Isakson <ikeman32@yahoo.com>
2020-08-05 15:32:06 -0700

    add resposiveness to videos

--- src/components/videos/Videos.js
+++ src/components/videos/Videos.js
@@ -1,16 +1,55 @@
 import React from 'react';
+import { layoutGenerator } from "react-break";
+import './Videos.scss';
 
 const Videos = () => {
-  /*Put all of your hooks, functions, etc here */
+  //configure responsiveness
+  const layout = layoutGenerator({
+    mobile: 0,
+    phablet: 550,
+    tablet: 768,
+    desktop: 992,
+  });
+
+  const OnMobile = layout.is("mobile");
+  const OnAtLeastTablet = layout.isAtLeast("tablet");
+  const OnAtMostPhablet = layout.isAtMost("phablet");
+  const OnDesktop = layout.is("desktop");
 
   return (
-    <div>
-      <h3>Walk Throughs</h3>
-      <iframe title='WalkThroughsPlayList' width="560" height="315" src="https://www.youtube-nocookie.com/embed/videoseries?list=PLg5BoFlx-HIakoUeSJ4x6QWA4YWU8gZcw" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-      <br></br>
-      <h3>Vlog</h3>
-      <iframe title='VlogPlayList' width="560" height="315" src="https://www.youtube-nocookie.com/embed/videoseries?list=PLg5BoFlx-HIbC1TNqMrQpCxYQGBSWhhj5" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-    </div>
+    <>
+      <OnAtLeastTablet></OnAtLeastTablet>
+      <OnAtMostPhablet></OnAtMostPhablet>
+      <OnMobile>
+        <div className='Mvideo-container'>
+          <div>
+            <h3>Walk Throughs</h3>
+            <iframe title='WalkThroughsPlayList' src="https://www.youtube-nocookie.com/embed/videoseries?list=PLg5BoFlx-HIakoUeSJ4x6QWA4YWU8gZcw" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+          </div>
+
+          <div>
+            <h3 >Vlog</h3>
+            <iframe title='VlogPlayList' src="https://www.youtube-nocookie.com/embed/videoseries?list=PLg5BoFlx-HIbC1TNqMrQpCxYQGBSWhhj5" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+          </div>
+
+        </div>
+      </OnMobile>
+      <OnDesktop>
+        <div className='video-container'>
+          <div>
+            <h3>Walk Throughs</h3>
+            <iframe title='WalkThroughsPlayList' src="https://www.youtube-nocookie.com/embed/videoseries?list=PLg5BoFlx-HIakoUeSJ4x6QWA4YWU8gZcw" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+          </div>
+
+          <div>
+            <h3 >Vlog</h3>
+            <iframe title='VlogPlayList' src="https://www.youtube-nocookie.com/embed/videoseries?list=PLg5BoFlx-HIbC1TNqMrQpCxYQGBSWhhj5" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+          </div>
+
+        </div>
+      </OnDesktop>
+    </>
+
   );
 };
 
