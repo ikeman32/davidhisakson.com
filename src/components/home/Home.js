e37373313c0742631973fd77e736bc0e83670418
David Isakson <ikeman32@yahoo.com>
2020-08-11 14:31:20 -0700

    Redo home page

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -29,142 +29,65 @@ const Home = () => {
       <OnMobile>
         <div className="Mhome_container">
           <section className="Mtop_panel">
-            <div id="Mapi">
-              <img src={api} alt="dukeapi"></img>
-              <h1>
-                <a href="https://www.npmjs.com/package/dukeapi">dukeapi</a>
-              </h1>
+          <div id="Mabout">
+            <h3>About the Author</h3>
+            <div className="Mimgbox">
+              <img src={me} alt="Me"></img>
             </div>
-            <div id="Mcustom_app">
-              <img src={logo} alt="Custom App"></img>
-              <h1>
-                <a href="https://www.npmjs.com/package/cra-template-duke">
-                  React Template
-                </a>
-              </h1>
-            </div>
-            <div id="Msignlingo">
-              <img src={signlingo} alt="Sign Lingo"></img>
-              <h1>
-                <a Href="https://github.com/Lambda-School-Labs/signlingo-be">
-                  Signlingo Backend
-                </a>
-              </h1>
-            </div>
+            <ul>
+              <li>
+                Graduate of <a href="https://lambdaschool.com/">Lambda School</a>
+              </li>
+              <li>US Army Veteran 1990 - 1998</li>
+              <li>
+                <Link id="MLink" to="/About">
+                  read more...
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div id="Mgithub">
+            <ul>
+              <li><img align="left" alt="ikeman32's Github Stats" src="https://github-readme-stats.vercel.app/api?username=ikeman32&show_icons=true&hide_border=true&theme=blue-green" /></li>
+            </ul>
+          </div>
           </section>
           <section className="Mbottom_panel">
-            <div id="Mblog">
-              <h3>Blog Posts</h3>
-              <details>
-                <summary>
-                  <a href="http://allthingsdev.tech/">From My Blog</a>
-                </summary>
-                <ul>
-                  <li>
-                    <a href="http://www.allthingsdev.tech/index.php/blog/create-a-react-app-template">
-                      Create a React App Template
-                    </a>
-                  </li>
-                  <li>
-                    <a href="http://www.allthingsdev.tech/index.php/blog/that-moment-when-things-start-to-click">
-                      That Moment When Things Start to Clic
-                    </a>
-                  </li>
-                  <li>
-                    <a href="http://www.allthingsdev.tech/index.php/blog/google-really-is-your-friend">
-                      Google Really is your Friend
-                    </a>
-                  </li>
-                  <li>
-                    <a href="http://www.allthingsdev.tech/index.php/blog/get-a-free-domain-name-and-free-hosting">
-                      Get a Free Domain Name and Free Hosting
-                    </a>
-                  </li>
-                  <li>
-                    <a href="http://www.allthingsdev.tech/index.php/blog/a-background-in-web-development-is-quite-useful">
-                      A Background in Web Development is Quite Useful
-                    </a>
-                  </li>
-                </ul>
-              </details>
-              <br></br>
-              <details>
-                <summary>
-                  <a href="https://dev.to/ikeman32">From Dev.to</a>
-                </summary>
-                <ul>
-                  <li>
-                    <a href="https://dev.to/ikeman32/boilerplate-for-a-custom-react-template-45hd">
-                      Boilerplate for a Custom React Template
-                    </a>
-                  </li>
-                </ul>
-              </details>
-              <br></br>
-              <details>
-                <summary>
-                  <a href="https://www.youtube.com/channel/UCL11jXWKQ0kY6ljqoTRY_mw">
-                    From My Youtube Channel
-                  </a>
-                </summary>
-                <ul>
-                  <li>
-                    <a href="https://www.youtube.com/watch?v=fuOMSySIq5I">
-                      Authenticating with Google using Okta Walk Through
-                    </a>
-                  </li>
-                  <li>
-                    <a href="https://www.youtube.com/watch?v=NrL9jYgAjcw">
-                      Have a RESTful API with JWT Built and Working in About a
-                      Minute
-                    </a>
-                  </li>
-                  <li>
-                    <a href="https://www.youtube.com/watch?v=3wqwaiKtiFA">
-                      Serve a React App Locally and Deploy to Your Website
-                    </a>
-                  </li>
-                </ul>
-              </details>
-            </div>
-            <div id="Mservices">
-              <ul>
-              <li><img align="left" alt="ikeman32's Github Stats" src="https://github-readme-stats.vercel.app/api?username=ikeman32&show_icons=true&hide_border=true&theme=blue-green" /></li>
-              </ul>
-            </div>
-            <div id="Mabout">
-              <h3>About the Author</h3>
-              <div className="Mimgbox">
-                <img src={me} alt="Me"></img>
-              </div>
-              <ul>
-                <li>
-                  Student at{" "}
-                  <a href="https://lambdaschool.com/">Lambda School</a>
-                </li>
-                <li>US Army Veteran</li>
-                <li>
-                  <Link id="MLink" to="/About">
-                    read more...
-                  </Link>
-                </li>
-              </ul>
-            </div>
+          <div id="mission">
+            <h3>To boldly code what no one has coded before!</h3>
+            <p>Languages and Tools</p>
+            <ul id='tech-stack'>
+              <li><img src="https://img.icons8.com/color/48/000000/visual-studio.png"/></li>
+              <li><img src="https://img.icons8.com/plasticine/48/000000/react.png"/></li>
+              <li><img src="https://img.icons8.com/dusk/48/000000/javascript-logo.png"/></li>
+              <li><img src="https://img.icons8.com/color/48/000000/python.png"/></li>
+              <li><img src="https://img.icons8.com/color/48/000000/nodejs.png"/></li>
+              <li><img src="https://img.icons8.com/nolan/48/html-filetype.png"/></li>
+              <li><img src="https://img.icons8.com/nolan/48/css-filetype.png"/></li>
+              <li><img src="https://img.icons8.com/color/48/000000/sass.png"/></li>
+              <li><img src="http://lesscss.org/public/img/less_logo.png" height='48px' width='48px'/></li>
+              <li><img src="https://img.icons8.com/color/48/000000/redux.png"/></li>
+              <li><img src="https://img.icons8.com/color/48/000000/git.png"/></li>
+              <li><img src="https://img.icons8.com/fluent/48/000000/github.png"/></li>
+              <li><img src="https://img.icons8.com/color/48/000000/linux.png"/></li>
+              <li><img src="https://avatars3.githubusercontent.com/u/13409222?s=200&v=4" height='48px' width='48px'/></li>
+            </ul>
+          </div>
           </section>
         </div>
       </OnMobile>
       <OnDesktop>
         <section className="top_panel">
-        <div id="about">
+          <div id="about">
             <h3>About the Author</h3>
             <div className="imgbox">
               <img src={me} alt="Me"></img>
             </div>
             <ul>
               <li>
-                Student at <a href="https://lambdaschool.com/">Lambda School</a>
+                Graduate of <a href="https://lambdaschool.com/">Lambda School</a>
               </li>
-              <li>US Army Veteran</li>
+              <li>US Army Veteran 1990 - 1998</li>
               <li>
                 <Link id="Link" to="/About">
                   read more...
@@ -172,89 +95,35 @@ const Home = () => {
               </li>
             </ul>
           </div>
-          <div id="services">
+          <div id="github">
             <ul>
               <li><img align="left" alt="ikeman32's Github Stats" src="https://github-readme-stats.vercel.app/api?username=ikeman32&show_icons=true&hide_border=true&theme=blue-green" /></li>
             </ul>
           </div>
         </section>
         <section className="bottom_panel">
-          <div id="blog">
-            <h3>Blog Posts</h3>
-            <details>
-              <summary>
-                <a href="http://allthingsdev.tech/">From My Blog</a>
-              </summary>
-              <ul>
-                <li>
-                  <a href="http://www.allthingsdev.tech/index.php/blog/create-a-react-app-template">
-                    Create a React App Template
-                  </a>
-                </li>
-                <li>
-                  <a href="http://www.allthingsdev.tech/index.php/blog/that-moment-when-things-start-to-click">
-                    That Moment When Things Start to Clic
-                  </a>
-                </li>
-                <li>
-                  <a href="http://www.allthingsdev.tech/index.php/blog/google-really-is-your-friend">
-                    Google Really is your Friend
-                  </a>
-                </li>
-                <li>
-                  <a href="http://www.allthingsdev.tech/index.php/blog/get-a-free-domain-name-and-free-hosting">
-                    Get a Free Domain Name and Free Hosting
-                  </a>
-                </li>
-                <li>
-                  <a href="http://www.allthingsdev.tech/index.php/blog/a-background-in-web-development-is-quite-useful">
-                    A Background in Web Development is Quite Useful
-                  </a>
-                </li>
-                <li>
-                  <a href="https://www.youtube.com/watch?v=3wqwaiKtiFA">
-                    Serve a React App Locally and Deploy to Your Website
-                  </a>
-                </li>
-              </ul>
-            </details>
-            <br></br>
-            <details>
-              <summary>
-                <a href="https://dev.to/ikeman32">From Dev.to</a>
-              </summary>
-              <ul>
-                <li>
-                  <a href="https://dev.to/ikeman32/boilerplate-for-a-custom-react-template-45hd">
-                    Boilerplate for a Custom React Template
-                  </a>
-                </li>
-              </ul>
-            </details>
-            <br></br>
-            <details>
-              <summary>
-                <a href="https://www.youtube.com/channel/UCL11jXWKQ0kY6ljqoTRY_mw">
-                  From My Youtube Channel
-                </a>
-              </summary>
-              <ul>
-                <li>
-                  <a href="https://www.youtube.com/watch?v=fuOMSySIq5I">
-                    Authenticating with Google using Okta Walk Through
-                  </a>
-                </li>
-                <li>
-                  <a href="https://www.youtube.com/watch?v=NrL9jYgAjcw">
-                    Have a RESTful API with JWT Built and Working in About a
-                    Minute
-                  </a>
-                </li>
-              </ul>
-            </details>
+          <div id="mission">
+            <h3>To boldly code what no one has coded before!</h3>
+            <p>Languages and Tools</p>
+            <ul id='tech-stack'>
+              <li><img src="https://img.icons8.com/color/64/000000/visual-studio.png"/></li>
+              <li><img src="https://img.icons8.com/plasticine/64/000000/react.png"/></li>
+              <li><img src="https://img.icons8.com/dusk/64/000000/javascript-logo.png"/></li>
+              <li><img src="https://img.icons8.com/color/48/000000/python.png"/></li>
+              <li><img src="https://nodejs.org/static/images/logo.svg" height='64' width='64'/></li>
+              <li><img src="https://img.icons8.com/nolan/64/html-filetype.png"/></li>
+              <li><img src="https://img.icons8.com/nolan/64/css-filetype.png"/></li>
+              <li><img src="https://img.icons8.com/color/64/000000/sass.png"/></li>
+              <li><img src="http://lesscss.org/public/img/less_logo.png" height='64px' width='64px'/></li>
+              <li><img src="https://img.icons8.com/color/64/000000/redux.png"/></li>
+              <li><img src="https://img.icons8.com/color/64/000000/git.png"/></li>
+              <li><img src="https://img.icons8.com/fluent/64/000000/github.png"/></li>
+              <li><img src="https://img.icons8.com/color/64/000000/linux.png"/></li>
+              <li><img src="https://avatars3.githubusercontent.com/u/13409222?s=200&v=4" height='64px' width='64px'/></li>
+            </ul>
           </div>
-          
-          
+
+
         </section>
       </OnDesktop>
     </div>
